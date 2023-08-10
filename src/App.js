19c5327062894cf544bc6e6ddaab821c60794e8e
Tracy Mumbi <tracychiuri@gmail.com>
2023-08-10 19:50:46 +0300

    learning rc-progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Line, Circle } from 'rc-progress';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 20, height: 310, width: 130 }}>
+      <Circle 
+      percent={10} 
+      strokeWidth={10} //thickness of the percentage circle
+      strokeColor="blue" //color of the percentage
+      trailColor='lightblue' //color of the remaining circle
+      trailWidth={10} // thickness of the remaining circle
+      strokeLinecap='square' 
+      />
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
